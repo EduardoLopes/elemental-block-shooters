9e7c34748c1f6f3d367f182ca0ecd5dd772eaa79
Eduardo Lopes <EduardoLopesDS@gmail.com>
2014-09-07 13:36:22 -0300

    Mouse script back to a single file

--- app/js/namespace.js
+++ app/js/namespace.js
@@ -45,32 +45,3 @@ function random( min, max ) {
 function randomChoice(array){
   return array[ random( 0, array.length - 1 ) ];
 }
-
-/* MOUSE */
-
-Game.mouse = {
-  x: 0,
-  y: 0,
-  angle: 0,
-  down: false
-};
-
-document.addEventListener('mousemove', function(e) {
-  var rect = Game.c1.getBoundingClientRect();
-
-  Game.mouse.x = e.clientX - rect.left;
-  Game.mouse.y = e.clientY - rect.top;
-
-});
-
-document.addEventListener('mousedown', function(e) {
-
-  Game.mouse.down = true;
-
-});
-
-document.addEventListener('mouseup', function(e) {
-
-  Game.mouse.down = false;
-
-});
